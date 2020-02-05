68718503461fcf24f81313119be7716a15a39b1b
JulioCezar_1991 <JulioCezar_1991@hotmail.com>
2020-02-05 17:00:13 -0300

    singletons

--- bin/11-Singletons/1-Static/Static.dart
+++ bin/11-Singletons/1-Static/Static.dart
@@ -1,5 +1,4 @@
 /* 
-<<<<<<< HEAD
   * Singleton
   - E um padrão de projeto que exista apenas uma instãcia da classe.
   - O contrutor nomeado privado sem construtor default impede de instanciar a classe.
@@ -23,31 +22,3 @@ void main() {
   print(identical(p1, p2));
   print(p2 == PessoaStatic.instancia);
 }
-=======
-  *Singleton
-  - É um padrão de projeto para que exista apenas uma instancoa na classe
-  - O construtor nomeado privado sem o contrutor default impede a instancia de classes
-
-  *Static
-  -A instancia pode ser acessada diretamente
- */
-
-import 'PessoaStatic.dart';
-
-void main(){
-
-  PessoaStatic.instancia.nome = 'Julio';
-  print('${PessoaStatic.instancia.nome}\n');
-
-  var p1 = PessoaStatic.instancia;
-  p1.nome = 'Laynna';
-  print(PessoaStatic.instancia.nome);
-
-  var p2 = PessoaStatic.instancia;
-  p2.nome = 'Neide';
-  print(PessoaStatic.instancia.nome);
-  print(identical(p1,p2)); // Compara se são iguais
-
-}
-
->>>>>>> f643af1f133770980410c9cbc280fac30361d5f4
